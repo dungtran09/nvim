0e8d0eb2b7280e93ec88d3f1a224187f72bfcd42
Dũng Trần <ngaynangnhe@gmail.com>
2025-01-25 23:29:38 +0700

    Update: Colorscheme.

--- lua/dungqt/plugins/colorscheme.lua
+++ lua/dungqt/plugins/colorscheme.lua
@@ -1,8 +1,13 @@
 return {
-	"LazyVim/LazyVim",
-	lazy = false,
-	priority = 1000,
+	-- "LazyVim/LazyVim",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	-- 	vim.cmd("colorscheme default")
+	-- end,
+	"rose-pine/neovim",
+	name = "rose-pine",
 	config = function()
-		vim.cmd("colorscheme default")
+		vim.cmd("colorscheme rose-pine")
 	end,
 }
